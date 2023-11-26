e2cee8cc31961c395d407f662dfdd3261adbca99
ricky septiaji <rickyseptiaji2k00@gmail.com>
2023-11-26 09:00:38 +0700

    project magang

---
+++ backend/routes/UserRoutes.js
@@ -0,0 +1,11 @@
+import express from 'express';
+import {createUser, deleteUser, getUsers, getUsersById, updateUser} from '../controllers/UserControllers.js';
+
+const router = express.Router();
+router.get('/users', getUsers);
+router.get('/users/:id', getUsersById);
+router.post('/users', createUser);
+router.patch('/users/:id', updateUser);
+router.delete('/users/:id', deleteUser);
+
+export default router;
